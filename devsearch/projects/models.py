d42fd4a8f2031859d95acf9e9a0635886ec3644b
Alex Dzehil <dirolex33@mail.ru>
2021-07-30 17:50:58 +0300

    Fixed style errors

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -10,11 +10,7 @@ class Project(models.Model):
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
-<<<<<<< HEAD
                           primary_key=True, editable=False)
-=======
-                    primary_key=True, editable=False)
->>>>>>> 1e45d0d7f257b304fda1eea76fef1544c53a4d41
 
     def __str__(self):
         return self.title
