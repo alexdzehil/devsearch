7209391e1e2cbe063b4f1e3fb8800a81fe66ab65
Alex Dzehil <dirolex33@mail.ru>
2021-08-12 23:59:26 +0300

    Add forms

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Project
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -14,3 +15,9 @@ def project(request, pk):
     project = Project.objects.get(id=pk)
     context = {'project': project}
     return render(request, 'projects/single-project.html', context)
+
+
+def create_project(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
