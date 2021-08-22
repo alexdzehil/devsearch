37bac09a205700c78c9a0c496ae3cc42e2963ba7
Alex Dzehil <dirolex33@mail.ru>
2021-08-23 01:16:49 +0300

    Added style login and register page

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -35,7 +35,7 @@ def login_page(request):
 
 def logout_user(request):
     logout(request)
-    messages.error(request, 'User was logged out')
+    messages.info(request, 'User was logged out')
     return redirect('login')
 
 
