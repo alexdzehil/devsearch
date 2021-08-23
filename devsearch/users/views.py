4ac5d753949e53dbd39b67b637272cc6fe76f1a7
Alex Dzehil <dirolex33@mail.ru>
2021-08-23 21:28:56 +0300

    Added search projects, search profiles

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
-from .models import Profile
+from .models import Profile, Skill
+from .utils import search_profiles
 
 
 def login_page(request):
@@ -61,8 +63,8 @@ def register_user(request):
 
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    profiles, search_query = search_profiles(request)
+    context = {'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
 
