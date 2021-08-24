9ed1c6c48941836cf61fb68ad76feeada4690af2
Alex Dzehil <dirolex33@mail.ru>
2021-08-24 17:07:38 +0300

    BugFix search + pagination

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -64,7 +64,7 @@ def register_user(request):
 
 def profiles(request):
     profiles, search_query = search_profiles(request)
-    custom_range, profiles = paginator_profiles(request, profiles, 3)
+    custom_range, profiles = paginator_profiles(request, profiles, 6)
 
     context = {'profiles': profiles, 'search_query': search_query, 'cestom_range': custom_range}
     return render(request, 'users/profiles.html', context)
